70032bae1001e38f44435d4965ea5eaef1d44119
Dmitry Zhukov <zhukov.dima.40@gmail.com>
2019-09-25 21:33:19 +0300

    fix listen method in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,4 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, "Server is running on port 3000");
+app.listen(3000, () => console.log("Server is running on port 3000"));
